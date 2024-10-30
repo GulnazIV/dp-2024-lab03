e82ddef6db761d038ddeda6021739924d635b3a6
Вахитова Гульназ <gulnaz.vakhitova.00@mail.ru>
2024-10-30 12:16:39 +0500

    feat(base64_decorator): Реализован класс декоратор для кодирования сообщения в base64

---
+++ components/decorators/base64_decorator.py
@@ -0,0 +1,39 @@
+import base64
+
+from components.decorators.message_decorator import MessageDecorator
+from interfaces.imessage import IMessage
+
+
+class Base64Decorator(MessageDecorator):
+    """
+    Декоратор для кодирования сообщения в формате Base64.
+    """
+
+    def __init__(self, message: IMessage) -> None:
+        """
+        Инициализация декоратора Base64.
+        :param message: Объект IMessage, который будет декорирован.
+        """
+        super().__init__(message)
+
+    def print_message(self) -> None:
+        """
+        Печатает закодированное сообщение в формате Base64.
+        """
+        self._covert_base64()
+        print(self.base64_encoded_message)
+
+    def get_content(self) -> str:
+        """
+        Получает содержимое декорируемого сообщения.
+        :return: Строка, представляющая содержимое основного сообщения.
+        """
+        return self.message.get_content()
+
+    def _covert_base64(self) -> None:
+        """
+        Преобразует сообщение.
+        """
+        self.base64_encoded_message = base64.b64encode(
+            self.get_content().encode()
+        ).decode("utf-8")
