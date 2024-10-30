0d439e982f7c2cdaeb758779a4060269ec1e59e9
Вахитова Гульназ <gulnaz.vakhitova.00@mail.ru>
2024-10-30 12:15:57 +0500

    feat(header_decorator): Реализован класс декоратор для заголовка

---
+++ components/decorators/header_decorator.py
@@ -0,0 +1,31 @@
+from components.decorators.message_decorator import MessageDecorator
+from interfaces.imessage import IMessage
+
+
+class HeaderDecorator(MessageDecorator):
+    """
+    Декоратор для добавления заголовка к сообщению.
+    """
+
+    def __init__(self, message: IMessage, header: str) -> None:
+        """
+        Инициализация декоратора заголовка.
+        :param message: Объект IMessage, который будет декорирован.
+        :param header: Заголовок, который будет добавлен к сообщению.
+        """
+        super().__init__(message)
+        self.header = header
+
+    def print_message(self) -> None:
+        """
+        Печатает заголовок и основное сообщение.
+        """
+        print(self.header)
+        self.message.print_message()
+
+    def get_content(self) -> str:
+        """
+        Получает содержимое сообщения с добавленным заголовком.
+        :return: Строка, содержащая заголовок и основное сообщение.
+        """
+        return f"{self.header}\n" + self.message.get_content()
